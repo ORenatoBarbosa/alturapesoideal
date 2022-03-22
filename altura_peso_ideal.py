cdd2fcc62868a12a8c7909dacccdf5abb77b6579
Renato Barbosa <98436200+ORenatoBarbosa@users.noreply.github.com>
2022-03-22 20:58:38 -0300

    Update and rename .gitignore to altura_peso_ideal.py

---
+++ altura_peso_ideal.py
@@ -0,0 +1,7 @@
+# Altura e Peso Ideal
+
+altura = float(input("Digite sua altura em metros: "))
+peso_ideal = (72.7 * altura) - 58
+print(f"O peso ideal para sua altura é: {peso_ideal:.2f}Kg")
+
+# Renato Barbosa
